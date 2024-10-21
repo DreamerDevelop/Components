8e7a5aaf78061d9d32d8aa7ffcdc5f913ae2cd81
DreamerDevelop <alackbarsenpai@gmail.com>
2024-10-21 19:31:00 +0200

    ej2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Provider as PaperProvider, Text } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Provider as PaperProvider, Text, TextInput, HelperText } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
 
 const nom = (textAMostrar, estil) => {
   return (
@@ -10,10 +10,46 @@ const nom = (textAMostrar, estil) => {
   );
 };
 
+const EntradaDEmail = ({ nom, instrucciones }) => {
+  const [email, setEmail] = useState('');
+  const [valido, setEsvalido] = useState(false);
+
+  const validarEmail = (email) => {
+    const emailRegex = /^[a-zA-Z]+\@[a-zA-Z\_\-0-9]+\.[a-z]{2,5}$/;
+
+    setEsvalido(emailRegex.test(email));
+  };
+
+  return (
+    <View>
+      <TextInput
+        label={nom}
+        placeholder={instrucciones}
+        value={email}
+        onChangeText={(text) => {
+          setEmail(text);
+          validarEmail(text);
+        }}
+        keyboardType="email-address"
+        right={<TextInput.Icon name={valido ? "check" : "close"} color={valido ? "green" : "red"} />}
+        underlineColor={!valido ? "red" : "green"}
+        activeUnderlineColor={!valido ? "red" : "green"}
+        error={!valido}
+      />
+      <HelperText type="error" visible={!valido}>
+        {valido ? '' : 'L\'email no és vàlid'}
+      </HelperText>
+    </View>
+  );
+};
+
 const App = () => {
   return (
     <PaperProvider>
-      {nom('Hugo y Manu', styles.estilDeText)}
+      <View style={styles.container}>
+        {nom('Hugo y Manu', styles.estilDeText)}
+        <EntradaDEmail nom="Correu electrònic" instrucciones="Introdueix el teu correu" />
+      </View>
     </PaperProvider>
   );
 };
@@ -23,6 +59,9 @@ const styles = StyleSheet.create({
     color: 'blue',
     fontSize: 25,
     fontWeight: 'bold',
+  },
+  container: {
+    padding: 20,
   },
 });
 
